8fa1b89776064bb59a67289fe04f8b830aefd4b6
cnmengkang <cnmengkang@gmail.com>
2023-01-09 09:37:05 +0800

    comapny

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import router from "./router";
 import store from "./store";
 import Element from 'element-ui'
 import Cookies from 'js-cookie'
-import { Message } from 'element-ui'
 import '@/static/icon/icon.css'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/static/styles/style.css'
 import '@/static/styles/reset.css'
 // 全局css样式表
-import { parseTime, formatTime, numCount,formatCurrentTime} from '@/utils/formdate'
+import { parseTime, formatTime, numCount, formatCurrentTime } from '@/utils/formdate'
 
 // 全局方法挂载
 // 时间日期格式化
@@ -21,11 +20,8 @@ Vue.prototype.numCount = numCount
 Vue.prototype.formatCurrentTime = formatCurrentTime
 
 
-// 全局弹窗
-Vue.prototype.$message = Message;
 Vue.config.productionTip = false;
 
-// bus 全局线程
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
 })
